7c042748a21885a6c0ef8029f9d30a1e39f28b80
Christopher Zündorf <christopher.zuendorf@foxium.de>
2025-05-23 20:52:01 +0200

    update cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import "./lib/db";
 import express from "express";
+import cors from "cors";
 import countryRoutes from "./routes/country";
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+app.use(cors({
+  origin: "https://kemmentaler-production.up.railway.app",
+}));
 
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
